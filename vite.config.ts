5d621b68c56435834a03c905f178b200177e9474
Awakened-Redstone <40528665+Awakened-Redstone@users.noreply.github.com>
2024-10-29 20:18:43 -0300

    chore: Use the environment variable for the base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,21 @@ import react from '@vitejs/plugin-react'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    TanStackRouterVite(),
-    react()
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({mode}) => {
+  const baseUrl = process.env.CF_PAGES_URL || mode === "development" ? "http://localhost:3000" : "https://sharedmodhelper.live";
+
+  return {
+    plugins: [
+      TanStackRouterVite(),
+      react()
+    ],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
+    define: {
+      'import.meta.env.BASE_URL': JSON.stringify(baseUrl)
+    }
+  }
 })
